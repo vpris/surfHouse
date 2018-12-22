57e251c15d03a4931c00d95d2c7c372f5288dd54
vpris <vpris@MacBook-Pro-vpris.local>
2018-12-22 16:49:53 +0600

    two

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+let n = parseInt(prompt('Первое число'));
+let s = parseInt(prompt('Второе число'));
+alert('Ответ: ' + (n - s));
---
+++ assets/js/script1.js
@@ -0,0 +1,27 @@
+let yourBudget = "Ваш бюджет?";
+let yourMarketName = "Название вашего магазина?";
+
+console.log(yourBudget);
+console.log(yourMarketName);
+
+let mainList = {
+    budget: 1000,
+    marketName: "Glory Hole",
+    shopGoods: [],
+    employers: {
+        id_1: 'Alex',
+        id_2: 'Serhio',
+        id_3: 'Artemio Petukhov'
+    },
+    open: true
+};
+
+prompt("Какой тип товара будем продавать?" . push(mainList.shopGoods));
+prompt("Какой тип товара будем продавать?");
+prompt("Какой тип товара будем продавать?");
+
+console.log('shopGoods');
+
+
+
+
